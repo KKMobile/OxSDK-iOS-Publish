72a4bc84cede39a8b7d1ec082685ec5eeb59f49d
耿志向 <gengzhixiang@hotmail.com>
2023-03-30 14:33:49 +0800

    [OMFacebookAdpter] om-facebook-6.12.0.0-202303301433

---
+++ FrameworkOxMediation/adapters/OMFacebookAdapter.framework/Headers/OMFacebookBanner.h
@@ -0,0 +1,15 @@
+#import <UIKit/UIKit.h>
+#import <FBAudienceNetwork/FBAudienceNetwork.h>
+#import "OMBannerCustomEvent.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OMFacebookBanner : UIView <OMBannerCustomEvent,FBAdViewDelegate>
+@property (nonatomic, strong) FBAdView *facebookBannerView;
+@property(nonatomic, weak, nullable) id<bannerCustomEventDelegate> delegate;
+- (instancetype)initWithFrame:(CGRect)frame adParameter:(NSDictionary *)adParameter rootViewController:(UIViewController *)rootViewController;
+- (void)loadAdWithLocalExtraParameter:(NSDictionary<NSString *,id> *)localExtraParameter;
+- (void)loadAdWithBidPayload:(NSString *)bidPayload localExtraParameter:(NSDictionary<NSString *,id> *)localExtraParameter;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FrameworkOxMediation/adapters/OMFacebookAdapter.framework/Headers/OMFacebookInterstitial.h
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+#import <FBAudienceNetwork/FBAudienceNetwork.h>
+#import "OMInterstitialCustomEvent.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OMFacebookInterstitial : NSObject <OMInterstitialCustomEvent,FBInterstitialAdDelegate>
+@property (nonatomic, strong) FBInterstitialAd *facebookInterstitial;
+@property (nonatomic, copy) NSString *pid;
+@property (nonatomic, weak) id<interstitialCustomEventDelegate> delegate;
+@property (nonatomic, assign) BOOL ready;
+@property (nonatomic, getter=isAdValid, readonly) BOOL adValid;
+
+- (instancetype)initWithParameter:(NSDictionary*)adParameter;
+- (void)loadAdWithLocalExtraParameter:(NSDictionary<NSString *,id> *)localExtraParameter;
+- (void)loadAdWithBidPayload:(NSString *)bidPayload localExtraParameter:(NSDictionary<NSString *,id> *)localExtraParameter;
+- (BOOL)isReady;
+- (void)show:(UIViewController*)vc;
+
+@end
+
+NS_ASSUME_NONNULL_END
