e76836cb2513213623c1c90cab569ee14596fc79
耿志向 <gengzhixiang@hotmail.com>
2023-03-30 15:19:07 +0800

    [OMIronSourceAdapter] om-ironSource-7.2.5.1.0-202303301517

---
+++ FrameworkOxMediation/adapters/OMIronSourceAdapter.framework/Headers/OMIronSourceRouter.h
@@ -0,0 +1,32 @@
+// Copyright 2020 ADTIMING TECHNOLOGY COMPANY LIMITED
+// Licensed under the GNU Lesser General Public License Version 3
+
+#import <Foundation/Foundation.h>
+#import "OMIronSourceClass.h"
+#import "OMIronSourceAdapter.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol OMIronSourceAdapterDelegate <NSObject>
+
+- (void)OMIronSourceDidReceiveReward;
+- (void)OMIronSourceDidload;
+- (void)OMIronSourceDidFailToLoad:(NSError*)error;
+- (void)OMIronSourceDidStart;
+- (void)OMIronSourceDidClick;
+- (void)OMIronSourceVideoEnd;
+- (void)OMIronSourceDidFinish;
+- (void)OMIronSourceDidFailToShow:(NSError *)error;
+@end
+
+@interface OMIronSourceRouter : NSObject<ISDemandOnlyInterstitialDelegate,ISDemandOnlyRewardedVideoDelegate>
+
+@property (nonatomic, strong) NSMapTable *placementDelegateMap;
+
++ (instancetype)sharedInstance;
+- (void)registerPidDelegate:(NSString*)pid delegate:(id)delegate;
+- (BOOL)isCanLoad:(id)adpter pid:(NSString *)pid;
+
+@end
+
+NS_ASSUME_NONNULL_END
