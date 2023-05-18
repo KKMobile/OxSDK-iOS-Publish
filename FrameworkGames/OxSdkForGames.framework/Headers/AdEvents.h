e74f5cf10f25d5fd2c5a1273053e879addee0872
耿志向 <543097079@qq.com>
2023-05-18 10:14:46 +0800

    [OxSdkForGames] Release:1.1.2.0-Ads01-202305181013

--- FrameworkGames/OxSdkForGames.framework/Headers/AdEvents.h
+++ FrameworkGames/OxSdkForGames.framework/Headers/AdEvents.h
@@ -90,6 +90,7 @@ static NSString * const EVENT_TOP40 = @"AdLTV_OneDay_Top40Percent";
 static NSString * const EVENT_TOP50 = @"AdLTV_OneDay_Top50Percent";
 static NSString * const EVENT_TOP60 = @"AdLTV_OneDay_Top60Percent";
 static NSString * const EVENT_TOTAL_ADS_REVENUE_001 = @"Total_Ads_Revenue_001";
+static NSString * const EVENT_TOTAL_ADS_REVENUE_01 = @"Total_Ads_Revenue_01";
 static NSString * const EVENT_AD_IMPRESSION_REVENUE = @"Ad_Impression_Revenue";
 static NSString * const EVENT_AD_MEMORY_LIMITED = @"Ad_Memory_Limited";
 static NSString * const EVENT_AD_REQUEST = @"Ad_Request";
