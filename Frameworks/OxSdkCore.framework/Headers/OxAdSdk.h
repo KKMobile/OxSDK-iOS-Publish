5546b4f851072e5dba76c2141dd863c494c1b82b
耿志向 <543097079@qq.com>
2023-08-08 18:15:42 +0800

    Release: 202308081830

--- Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
+++ Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "OxAdSdkManager.h"
 
-#define OxSDK_VERSION    @"1.2.0.0-RC03-202308041700"
+#define OxSDK_VERSION    @"1.2.0.0-RC03-202308081830"
 
 NS_ASSUME_NONNULL_BEGIN
 
