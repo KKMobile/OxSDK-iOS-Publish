b365eab7b592b5eeb876422b61660c6dbb33d804
耿志向 <543097079@qq.com>
2023-09-26 15:46:00 +0800

    [OxSdkForGames] Release:1.1.3.0

--- FrameworkGames/OxSdkForGames.framework/Headers/OxConsentMananger.h
+++ FrameworkGames/OxSdkForGames.framework/Headers/OxConsentMananger.h
@@ -28,11 +28,10 @@ typedef void (^GDPRDismiss)(void);
 /// - Parameters:
 ///   - defaultGDPRTool: 默认使用的 GDPR 平台
 ///   - privacyPolicyLink: Max GDPR 的隐私政策链接
-///   - consentCheckResultCallback: 是否受到GDPR影响回到
-- (void)initialize:(GDPRTool)defaultGDPRTool privacyPolicyLink:(NSString *)privacyPolicyLink consentCheckResultCallback:(void (^)(BOOL isSubjectGDPR))consentCheckResultCallback;
+///   - consentCheckResultCallback: 是否受到GDPR影响回调
+- (void)initialize:(GDPRTool)defaultGDPRTool privacyPolicyLink:(NSString *)privacyPolicyLink consentCheckResultCallback:(void (^)(BOOL isSubjectToGDPR))consentCheckResultCallback;
 
-/// 展示 GDPRUI
-/// ATT请求回调里面调用不要判断ATT状态
+/// 展示 GDPRUI  展示之前不用判断 isSubjectToGDPR
 /// - Parameters:
 ///   - viewController: 需要展示的界面
 ///   - force: 是否为设置界面 (YES=设置界面)
@@ -40,7 +39,7 @@ typedef void (^GDPRDismiss)(void);
 - (BOOL)showConsentDialog:(UIViewController *)viewController force:(BOOL)force dismiss:(nullable GDPRDismiss)dismiss;
 
 /// 是否是可以展示GDPR的地区/国家
-- (BOOL)isSubjectGDPR;
+- (BOOL)isSubjectToGDPR;
 
 /// Max 下 SDK 初始化成功
 - (void)onMediationInitialized:(Platform)mediation;
