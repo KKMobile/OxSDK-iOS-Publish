dbe0f6081df2ff8b964819396f18dd29fa603ff1
耿志向 <543097079@qq.com>
2024-03-26 16:37:45 +0800

    [OxSdkForGames] 去掉无用的头文件 Release:1.1.3.5-MO-LP-202403261640

--- FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
+++ FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
@@ -9,7 +9,7 @@
 #import "DataTools.h"
 #import "AdEventDelegate.h"
 
-#define OXSDK_VERSION "1.1.3.5-MO-LP-202403251830"
+#define OXSDK_VERSION "1.1.3.5-MO-LP-202403261640"
 
 NS_ASSUME_NONNULL_BEGIN
 
