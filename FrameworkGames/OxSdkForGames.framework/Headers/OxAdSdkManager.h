302406db117a3517bb3bab8c0d7055f1676dc2ad
耿志向 <543097079@qq.com>
2024-03-28 16:45:24 +0800

    [OxSdkForGames] Release:1.1.3.5-MO-LP-202403281700 处理Banner 位置异常的bug

--- FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
+++ FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
@@ -9,7 +9,7 @@
 #import "DataTools.h"
 #import "AdEventDelegate.h"
 
-#define OXSDK_VERSION "1.1.3.5-MO-LP-202403261640"
+#define OXSDK_VERSION "1.1.3.5-MO-LP-202403281700"
 
 NS_ASSUME_NONNULL_BEGIN
 
