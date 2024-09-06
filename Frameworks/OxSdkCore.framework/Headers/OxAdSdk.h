d21242ced972d50eb9e81e088c394cc9f1d6fbcc
耿志向 <543097079@qq.com>
2024-09-06 20:05:47 +0800

    [OxSdkForTools] 1.2.1.1-RC01-202409062000

--- Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
+++ Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
@@ -10,7 +10,7 @@
 #import "OxAdSdkManager.h"
 #import "OxGamesEventUtil.h"
 
-#define OxSDK_VERSION    @"1.2.1.1-RC01-202409061600"
+#define OxSDK_VERSION    @"1.2.1.1-RC01-202409062000"
 
 NS_ASSUME_NONNULL_BEGIN
 
