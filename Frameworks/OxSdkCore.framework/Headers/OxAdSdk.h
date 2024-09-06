3d114b7102107b471070a022baff347a704635cf
耿志向 <543097079@qq.com>
2024-09-06 16:04:43 +0800

    [OxSdkForTools] Release:1.2.1.0-RC02-202409061600

--- Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
+++ Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
@@ -10,7 +10,7 @@
 #import "OxAdSdkManager.h"
 #import "OxGamesEventUtil.h"
 
-#define OxSDK_VERSION    @"1.2.1.0-RC02-202409052130"
+#define OxSDK_VERSION    @"1.2.1.0-RC02-202409061600"
 
 NS_ASSUME_NONNULL_BEGIN
 
