b169caf76af82a95df19c2361e70331cb2671b54
耿志向 <543097079@qq.com>
2024-09-14 17:22:41 +0800

    [OxSdkForTools] 2.0-MO-TMultiBidding-202409141740

--- Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
+++ Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
@@ -10,7 +10,7 @@
 #import "OxAdSdkManager.h"
 #import "OxGamesEventUtil.h"
 
-#define OxSDK_VERSION    @"2.0-MO-TMultiBidding-202409141730"
+#define OxSDK_VERSION    @"2.0-MO-TMultiBidding-202409141740"
 
 NS_ASSUME_NONNULL_BEGIN
 
