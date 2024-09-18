c550ca186350b0e12f1f75169a3919bf742196b8
耿志向 <543097079@qq.com>
2024-09-18 16:26:59 +0800

    [OxSdkForGames] 1.0-UA-AdjustIAP-202409181630

--- FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
+++ FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
@@ -9,7 +9,7 @@
 #import "DataTools.h"
 #import "AdEventDelegate.h"
 
-#define OXSDK_VERSION "1.1.4.1"
+#define OXSDK_VERSION "1.0-UA-AdjustIAP-202409181630"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -17,7 +17,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, assign) BOOL       tachiEnable; // 设置tachi是否启用，主要用于部分不需要打点的iOS的工具
 
-@property(nonatomic, strong) NSMutableArray<NSString *> *cacheLogMessage;
 @property(nonatomic, assign) BOOL    mAdmobSdkInitialed;
 @property(nonatomic, assign) BOOL    mMaxSdkInitialed;
 @property(nonatomic, assign) BOOL    enableDebug;
@@ -69,8 +68,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// RemoteConfig 获取完成。
 - (void)onRemoteConfigFetchCompleted;
-
-- (NSURL *)getCacheLog;
 
 @end
 
