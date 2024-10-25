c4809089810e30949130a2735de0bb783cc2ed46
耿志向 <543097079@qq.com>
2024-10-25 12:16:49 +0800

    [Fyber] 8.3.2.0

---
+++ Frameworks/OxFyberAdapter.framework/Headers/OxFyberAdapter.h
@@ -0,0 +1,21 @@
+//
+//  OxFyberAdapter.h
+//  OxFyberAdapter
+//
+//  Created by Mavl_2023_100272 on 2024/10/25.
+//  Copyright © 2024 耿志向. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <OxFyberAdapter/OxCustomFyberAdapter.h>
+
+
+//! Project version number for OxFyberAdapter.
+FOUNDATION_EXPORT double OxFyberAdapterVersionNumber;
+
+//! Project version string for OxFyberAdapter.
+FOUNDATION_EXPORT const unsigned char OxFyberAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OxFyberAdapter/PublicHeader.h>
+
+
