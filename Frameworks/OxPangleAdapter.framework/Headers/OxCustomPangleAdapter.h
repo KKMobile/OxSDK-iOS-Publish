214dd00bb4941fc584a484a4366a3f1a98aa1060
耿志向 <543097079@qq.com>
2024-10-25 14:56:22 +0800

    [Pangle] 6.2.0.9.0

---
+++ Frameworks/OxPangleAdapter.framework/Headers/OxCustomPangleAdapter.h
@@ -0,0 +1,13 @@
+//
+//  OxCustomPangleAdapter.h
+//  Adapters
+//
+//  Created by Thomas So on 12/25/18.
+//  Copyright © 2018 AppLovin. All rights reserved.
+//
+
+#import <AppLovinSDK/AppLovinSDK.h>
+
+@interface OxCustomPangleAdapter : ALMediationAdapter <MASignalProvider, MAInterstitialAdapter, MAAppOpenAdapter, MARewardedAdapter, MAAdViewAdapter, MANativeAdAdapter>
+
+@end
---
+++ Frameworks/OxPangleAdapter.framework/Headers/OxPangleAdapter.h
@@ -0,0 +1,20 @@
+//
+//  OxPangleAdapter.h
+//  OxPangleAdapter
+//
+//  Created by Mavl_2023_100272 on 2024/10/25.
+//  Copyright © 2024 耿志向. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <OxPangleAdapter/OxCustomPangleAdapter.h>
+
+//! Project version number for OxPangleAdapter.
+FOUNDATION_EXPORT double OxPangleAdapterVersionNumber;
+
+//! Project version string for OxPangleAdapter.
+FOUNDATION_EXPORT const unsigned char OxPangleAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OxPangleAdapter/PublicHeader.h>
+
+
