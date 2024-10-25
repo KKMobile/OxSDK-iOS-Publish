7c39ebdf3a964564e4a4085f9843866aa491b5f3
耿志向 <543097079@qq.com>
2024-10-25 16:45:04 +0800

    [UnityAds] 4.12.3.0

---
+++ Frameworks/OxUnityAdsAdapter.framework/Headers/OxUnityAdsAdapter.h
@@ -0,0 +1,19 @@
+//
+//  OxUnityAdsAdapter.h
+//  OxUnityAdsAdapter
+//
+//  Created by Mavl_2023_100272 on 2024/10/25.
+//  Copyright © 2024 耿志向. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OxUnityAdsAdapter.
+FOUNDATION_EXPORT double OxUnityAdsAdapterVersionNumber;
+
+//! Project version string for OxUnityAdsAdapter.
+FOUNDATION_EXPORT const unsigned char OxUnityAdsAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OxUnityAdsAdapter/PublicHeader.h>
+
+
