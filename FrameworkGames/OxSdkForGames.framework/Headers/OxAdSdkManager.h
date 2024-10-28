30ad2a84eb5c49156703746c410f7eb42bdbfd7b
耿志向 <543097079@qq.com>
2024-10-28 16:21:55 +0800

    [OxSdkForGames] 2.0-UA-AdjustIAP-S2S-20241015-1141

--- FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
+++ FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
@@ -9,7 +9,7 @@
 #import "DataTools.h"
 #import "AdEventDelegate.h"
 
-#define OXSDK_VERSION "1.0-UA-AdjustIAP-S2S-20241015-1141"
+#define OXSDK_VERSION "2.0-UA-AdjustIAP-S2S-20241015-1141"
 
 NS_ASSUME_NONNULL_BEGIN
 
