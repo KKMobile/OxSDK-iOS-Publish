ccfa5f96a2dd73dcbdbac8805961c26eb72f1fac
耿志向 <543097079@qq.com>
2024-10-30 10:51:33 +0800

    [OxApsAdapter] 更新API

--- Frameworks/OxApsAdapter.framework/Headers/OxAPSAd.h
+++ Frameworks/OxApsAdapter.framework/Headers/OxAPSAd.h
@@ -9,12 +9,23 @@
 #import <Foundation/Foundation.h>
 #import <DTBiOSSDK/DTBiOSSDK.h>
 
+
+typedef NS_ENUM(NSInteger, OxAPSAdFormat) {
+    OxAPSAdFormatBanner = 0,       /* 320x50 */
+    OxAPSAdFormatMREC,         /* 300x250 */
+    OxAPSAdFormatLeaderBoard,  /* 728x90 */
+    OxAPSAdFormatInterstitial, /* full screen */
+    OxAPSAdFormatRewardedVideo, /* full screen */
+    OxAPSAdFormatInterstitialVideo /* video player width and height set by publisher, default is 320x480*/
+};
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface OxAPSAd : NSObject
-
-- (void)initApsSdk:(NSString *)appId testMode:(BOOL)testMode completion:(nullable void (^)(NSError *error))completion;
-- (void)loadApsAd:(NSString *)amazonAdSlotId adFormat:(APSAdFormat)adFormat ompletion:(nullable void (^)(BOOL success,id info)) completion;
++ (instancetype)sharedInstance;
++ (void)initApsSdk:(NSString *)appId testMode:(BOOL)testMode;
++ (BOOL)isReady;
+- (void)loadApsAd:(NSString *)amazonAdSlotId adFormat:(OxAPSAdFormat)adFormat ompletion:(nullable void (^)(BOOL success,id info)) completion;
 
 @end
 
