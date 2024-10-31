63a36661eab4993cf250a86b3389bf15e4cfdcdc
耿志向 <543097079@qq.com>
2024-10-31 16:38:00 +0800

    [OxSdkForGames]  1.3.0.0-RC01-202410311630

--- FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
+++ FrameworkGames/OxSdkForGames.framework/Headers/OxAdSdkManager.h
@@ -9,7 +9,7 @@
 #import <OxSdkForGames/DataTools.h>
 #import <OxSdkForGames/AdEventDelegate.h>
 
-#define OXSDK_VERSION "1.3.0.0-RC01-202410311000"
+#define OXSDK_VERSION "1.3.0.0-RC01-20241031630"
 
 NS_ASSUME_NONNULL_BEGIN
 
