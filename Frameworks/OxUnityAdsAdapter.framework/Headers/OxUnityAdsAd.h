add57dcea284326e1ee4a7e167463d25734baa61
耿志向 <543097079@qq.com>
2024-10-31 15:11:10 +0800

    a-unityads-4.12.3.0

---
+++ Frameworks/OxUnityAdsAdapter.framework/Headers/OxUnityAdsAd.h
@@ -0,0 +1,19 @@
+//
+//  OxUnityAdsAd.h
+//  OxUnityAdsAdapter
+//
+//  Created by Mavl_2023_100272 on 2024/10/25.
+//  Copyright © 2024 耿志向. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OxUnityAdsAd : NSObject
+
++ (void)setUnityAdsGdprConsent:(NSNumber *)isAccepted;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Frameworks/OxUnityAdsAdapter.framework/Headers/OxUnityAdsAdapter.h
+++ Frameworks/OxUnityAdsAdapter.framework/Headers/OxUnityAdsAdapter.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <OxUnityAdsAdapter/OxUnityAdsAd.h>
 
 //! Project version number for OxUnityAdsAdapter.
 FOUNDATION_EXPORT double OxUnityAdsAdapterVersionNumber;
