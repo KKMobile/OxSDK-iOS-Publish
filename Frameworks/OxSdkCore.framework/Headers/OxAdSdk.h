88a860efc787f510b4fa4b45ced7942ca470f36a
耿志向 <543097079@qq.com>
2024-11-07 17:29:23 +0800

    [OxSdk-Tools-iOS] 1.2.0.2

--- Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
+++ Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
@@ -10,7 +10,7 @@
 #import "OxAdSdkManager.h"
 #import "OxGamesEventUtil.h"
 
-#define OxSDK_VERSION    @"3.0-MO-TMultiBidding-202410091430"
+#define OxSDK_VERSION    @"1.2.0.2"
 
 NS_ASSUME_NONNULL_BEGIN
 
