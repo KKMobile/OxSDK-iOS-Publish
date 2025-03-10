1e32815457466e9098b8822b29dc9d22baab4ebd
耿志向 <543097079@qq.com>
2025-03-10 10:27:28 +0800

    新增 Max  Tradplus custom  adapter

---
+++ Frameworks/OxTradPlusAdapter.framework/Headers/OxTradPlusAdapter.h
@@ -0,0 +1,19 @@
+//
+//  OxTradPlusAdapter.h
+//  OxTradPlusAdapter
+//
+//  Created by Mavl_2023_100272 on 2025/3/6.
+//  Copyright © 2025 耿志向. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OxTradPlusAdapter.
+FOUNDATION_EXPORT double OxTradPlusAdapterVersionNumber;
+
+//! Project version string for OxTradPlusAdapter.
+FOUNDATION_EXPORT const unsigned char OxTradPlusAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OxTradPlusAdapter/PublicHeader.h>
+
+
