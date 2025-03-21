b36895d433da0a1dcdbe6c44f246a6065be9874b
耿志向 <543097079@qq.com>
2025-03-21 16:24:56 +0800

    更新版本号  1.2.2.0-RC01-202503211630

--- Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
+++ Frameworks/OxSdkCore.framework/Headers/OxAdSdk.h
@@ -10,7 +10,7 @@
 #import "OxAdSdkManager.h"
 #import "OxGamesEventUtil.h"
 
-#define OxSDK_VERSION    @"1.2.2.0-RC01-202503172130"
+#define OxSDK_VERSION    @"1.2.2.0-RC01-202503211630"
 
 NS_ASSUME_NONNULL_BEGIN
 
