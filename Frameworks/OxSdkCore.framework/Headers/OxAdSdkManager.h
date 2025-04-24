d3370f71eff91a7cc2b8f9f6ae692bacc40fea4b
耿志向 <543097079@qq.com>
2025-04-24 19:20:10 +0800

    [OxSdkCore] 2.0-PR-UVMTC-202504241930-1300

--- Frameworks/OxSdkCore.framework/Headers/OxAdSdkManager.h
+++ Frameworks/OxSdkCore.framework/Headers/OxAdSdkManager.h
@@ -9,7 +9,7 @@
 #import "AdEventDelegate.h"
 #import "DataTools.h"
 
-#define OXSDK_VERSION "1.0-PR-UVMTC-202504232350-1300"
+#define OXSDK_VERSION "2.0-PR-UVMTC-202504241930-1300"
 
 NS_ASSUME_NONNULL_BEGIN
 
