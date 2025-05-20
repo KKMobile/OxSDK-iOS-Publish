f88b30c8d945d040e7032dbe9b4f06f2ea087b63
耿志向 <543097079@qq.com>
2025-05-20 18:29:00 +0800

    [ironsource] 8.3.0.0.1

---
+++ Frameworks/OxIronsourceAdapter.framework/Headers/OxIronsourceAdapter.h
@@ -0,0 +1,19 @@
+//
+//  OxIronsourceAdapter.h
+//  OxIronsourceAdapter
+//
+//  Created by Mavl_2023_100272 on 2025/5/14.
+//  Copyright © 2025 耿志向. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OxIronsourceAdapter.
+FOUNDATION_EXPORT double OxIronsourceAdapterVersionNumber;
+
+//! Project version string for OxIronsourceAdapter.
+FOUNDATION_EXPORT const unsigned char OxIronsourceAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OxIronsourceAdapter/PublicHeader.h>
+
+
