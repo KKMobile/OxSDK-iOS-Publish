4c581dcf39a4e4debaa3272f9908c406054ddfa9
耿志向 <543097079@qq.com>
2025-10-21 14:26:21 +0800

    [core] 1.0-PR-MaxDynamic-202510161630-1330

--- Frameworks/OxSdkCore.framework/Headers/OxAdSdkManager.h
+++ Frameworks/OxSdkCore.framework/Headers/OxAdSdkManager.h
@@ -9,7 +9,7 @@
 #import "AdEventDelegate.h"
 #import "DataTools.h"
 
-#define OXSDK_VERSION "1.0-PR-MaxDynamic-202510161600-1330"
+#define OXSDK_VERSION "1.0-PR-MaxDynamic-202510161630-1330"
 
 NS_ASSUME_NONNULL_BEGIN
 
